cb3cdcdecf6af8cf7a4493db2bfca9914aaafc65
hamayj <zmlzml0307@gmail.com>
2022-07-19 21:45:45 +0900

    study: callback 복습

---
+++ 22년 07/7월 3주차/callbackEx1.js
@@ -0,0 +1,8 @@
+function introduce (lastName, firstName, callback) {
+	var fullName = lastName + firstName;
+	callback(fullName);
+}
+
+introduce("홍", "길동", function(name) {
+		console.log(name);
+	});
