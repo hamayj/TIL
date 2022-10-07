6b6d9adcd2bdc12a528f35148a4d6924cfdaee4a
hamayj <zmlzml0307@gmail.com>
2022-10-07 17:16:52 +0900

    study: useEffect예제1

--- 221007-hooks/src/Info.js
+++ 221007-hooks/src/Info.js
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+
+    useEffect(() => {
+        console.log("렌더링이 완료되었습니다.");
+        console.log(
+            name,nickname
+        );
+    }); // 왜 {} 처리 한걸까? 벗겨서 또 확인해보자.
+    // {} 각각의 변수를 담은 객체로 처리해서 보여주겠다. (키:값 형태)
+
 
     const onChangeName = e => {
         setName(e.target.value);
