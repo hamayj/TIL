c41e87cf197a16ac3cc6e06529d5f5dd3f5ad307
hamayj <zmlzml0307@gmail.com>
2022-10-07 17:51:45 +0900

    study: useEffect-특정 값 업데이트시 실행

--- 221007-hooks/src/Info.js
+++ 221007-hooks/src/Info.js
@@ -9,10 +9,12 @@ const Info = () => {
         // console.log(
         //     name,nickname
         // );
-        console.log("마운트 될 때만 실행됩니다.");
-    }, []); // 왜 {} 처리 한걸까? 벗겨서 또 확인해보자.
+        // console.log("마운트 될 때만 실행됩니다.");
+        console.log(name);
+    }, [name]); // 왜 {} 처리 한걸까? 벗겨서 또 확인해보자.
     // {} 각각의 변수를 담은 객체로 처리해서 보여주겠다. (키:값 형태)
     // 마운트 될 때만 실행하고 싶을 때는 useEffect의 두번째 파라미터로 빈 배열 넣어주면 됨.
+    // 배열 안에는 useState를 통해 관리하고 있는 상태를 넣어줘도 되고, props
 
     const onChangeName = e => {
         setName(e.target.value);
